d807b97357fc052bce06d8d268e9bff7f043a45a
Alex (Bluelight) <alex.ren2006@gmail.com>
2014-12-11 02:44:36 -0500

    update

--- conats/app/controllers/Application.java
+++ conats/app/controllers/Application.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
 import play.*;
 import play.mvc.*;
 
@@ -7,8 +9,21 @@ import views.html.*;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
+	public static Result index() {
+		return ok(index.render("Your new application is ready."));
+	}
+
+	@BodyParser.Of(BodyParser.Json.class)
+	public static Result typecheck() {
+		JsonNode json = request().body().asJson();
+		String code = json.findPath("code").textValue();
+		if (code == null) {
+			return badRequest("Missing parameter [name]");
+		} else {
+			Logger.info("code is " + code);
+			return ok("Hello " + code);
+		}
+
+	}
 
 }
--- conats/public/javascripts/root.js
+++ conats/public/javascripts/root.js
@@ -1,7 +1,7 @@
 
 
 angular.module("root", ['ui.bootstrap'])  // 
-.controller("index", ["$scope", function($scope) {
+.controller("index", ["$scope", "$http", function($scope, $http) {
     $scope.favoriteWord;
     $scope.favoriteColor;
     $scope.favoriteShape;
@@ -10,7 +10,14 @@ angular.module("root", ['ui.bootstrap'])  //
     	if (window.console) {
     	  console.log("root.typecheck!");
     	}
-    }
+    	$http.post('/ats/typecheck', {code: "ee"}).
+    	  success(function(data, status, headers, config) {
+    		  console.log("data is " + data);
+    	  }).
+    	  error(function(data, status, headers, config) {
+    		  console.log("error");
+    	  });
+    };
 }]);
 
 
